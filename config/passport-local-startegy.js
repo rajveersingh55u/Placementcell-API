d88676b889345c74fac82c6dd9492c3ee48b36b1
Rajveer Singh <132346507+rajveersingh55u@users.noreply.github.com>
2023-09-16 23:46:29 +0530

    Add files via upload

---
+++ config/passport-local-startegy.js
@@ -0,0 +1,77 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/userSchema');
+
+// const local = new LocalStrategy({ usernameField: 'email' }, function (
+//   email,
+//   password,
+//   done
+// ) {
+//   User.findOne({ email }, function (error, user) {
+//     if (error) {
+//       console.log(`Error in finding user: ${error}`);
+//       return done(error);
+//     }
+
+//     if (!user || !user.isPasswordCorrect(password)) {
+//       console.log('Invalid Username/Password');
+//       return done(null, false);
+//     }
+//     return done(null, user);
+//   });
+// });
+
+passport.use(new LocalStrategy({
+  usernameField : "email" 
+},
+async function(email,password,done){
+  const user = await User.findOne({email:email});
+  console.log(user);
+  if(!user){
+    console.log('Error in finding User');
+    return done(Error);
+  }
+  
+  if(!user || !user.isPasswordCorrect(password)){
+    console.log('Invalid Username/Password');
+    /* THERE IS NO ERROR BUT USER IS NOT FOUND THATS WHY SECOND PARAM IS FALSE */
+    return done(null,false)
+  }
+  return done(null,user);
+}
+
+))
+
+
+//serialize user
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+//deserialize user
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log('Error in finding user--> Passport');
+      return done(err);
+    }
+    return done(null, user);
+  });
+});
+
+// check if user is authenticated
+passport.checkAuthentication = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect('/users/signin');
+};
+
+// set authenticated user for views
+passport.setAuthenticatedUser = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
+  next();
+};
